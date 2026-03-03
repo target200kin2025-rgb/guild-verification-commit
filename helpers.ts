8fb0b98f4e548906b0b39d7d6cc1b419e31b81bc
target200kin2025-rgb <target200kin2025@gmail.com>
2026-03-03 22:06:50 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T19:06:14.675Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-03T19:06:49.914Z
+Commit: Fix broken links in documentation
