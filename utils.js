80c1587d94f33951ed13ccab46a27b535c3f282f
target200kin2025-rgb <target200kin2025@gmail.com>
2026-03-03 22:08:41 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T19:08:34.503Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-03T19:08:40.777Z
+Commit: Resolve null pointer exception in user service
